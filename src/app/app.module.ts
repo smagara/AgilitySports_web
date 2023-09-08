beba9a9c7cec95fff095d40b91651b59e40c806b
smagara <smagara@hotmail.com>
2023-09-08 16:13:45 -0400

    add pagination with browser animations for the roster grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'; 
 import { MegaMenuModule } from 'primeng/megamenu';
 
@@ -17,6 +18,7 @@ import { PgaModule } from './pga/pga.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MegaMenuModule,
     HttpClientModule,
     AppRoutingModule,
