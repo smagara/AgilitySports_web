b090a265886b26bef6416ad19ca78db801c7f835
Steve M <smagara@hotmail.com>
2025-08-01 13:03:05 -0400

    Cache the Position codes by Sport

--- src/app/staticdata/services/staticdata.service.ts
+++ src/app/staticdata/services/staticdata.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { PositionCodesDTO } from './positioncodes';
 
@@ -8,13 +9,24 @@ import { PositionCodesDTO } from './positioncodes';
   providedIn: 'root'
 })
 export class StaticDataService {
-  baseURL = environment.apiUrl;
+  private baseURL = environment.apiUrl;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  // In-memory cache: key is sport code, value is the positions array
+  private positionCache = new Map<string, PositionCodesDTO[]>();
+
+  constructor(private http: HttpClient) {}
 
   GetPositionCodes(sport: string): Observable<PositionCodesDTO[]> {
-    return this.http.get<PositionCodesDTO[]>(this.baseURL + 'staticdata/positions?sport=' + sport);
+    const cached = this.positionCache.get(sport);
+    if (cached) {
+      // Return cached value as observable, rather than calling the API again
+      return of(cached);
+    }
+
+    // Fetch from API and "tap" into the observable chain to cache the result for this sport
+    return this.http.get<PositionCodesDTO[]>(`${this.baseURL}staticdata/positions?sport=${sport}`)
+      .pipe(
+        tap(data => this.positionCache.set(sport, data))
+      );
   }
 }
