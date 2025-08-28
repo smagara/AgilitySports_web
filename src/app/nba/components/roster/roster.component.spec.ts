b9bbca118fc67604870f4acdb32617dbff0ef9f2
Steve M <smagara@hotmail.com>
2025-08-27 20:23:43 -0400

    Fix failing NBA CI/CD test

--- src/app/nba/components/roster/roster.component.spec.ts
+++ src/app/nba/components/roster/roster.component.spec.ts
@@ -1,10 +1,32 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, forwardRef, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
 import { NbaService } from '../../services/nba.service';
+import { RosterDetailComponent } from '../roster-detail/roster-detail.component';
+import { RosterListComponent } from '../roster-list/roster-list.component';
 import { RosterComponent } from './roster.component';
+
+// Mock PositionDropdownComponent
+@Component({
+  selector: 'app-position-dropdown',
+  template: '<select></select>',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MockPositionDropdownComponent),
+      multi: true
+    }
+  ]
+})
+class MockPositionDropdownComponent {
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+  setDisabledState(isDisabled: boolean): void {}
+}
 
 describe('RosterComponent', () => {
   let component: RosterComponent;
@@ -13,7 +35,12 @@ describe('RosterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RosterComponent],
+      declarations: [
+        RosterComponent,
+        RosterListComponent,
+        RosterDetailComponent,
+        MockPositionDropdownComponent
+      ],
       imports: [
         HttpClientTestingModule,
         ReactiveFormsModule,
@@ -21,7 +48,8 @@ describe('RosterComponent', () => {
         DialogModule,
         DropdownModule
       ],
-      providers: [NbaService]
+      providers: [NbaService],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     });
     fixture = TestBed.createComponent(RosterComponent);
     component = fixture.componentInstance;
